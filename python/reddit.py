af2de20e9ec161a85d412356734f00b279c7c5b4
darkskill33 <alanas.pausa@gmail.com>
2022-11-02 15:51:07 +0200

    sorting top 100 frequent words

--- python/reddit.py
+++ python/reddit.py
@@ -2,6 +2,7 @@ import praw
 import pandas as pd
 import numpy as np
 import matplotlib as plt
+import collections
 reddit = praw.Reddit(client_id="VxNJxCLCw8OIfB5PqrUIFg", 
                      client_secret="uRYevllCXluCxz5sqqbudMpZBkjBQw", 
                      user_agent="Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0")
@@ -14,20 +15,21 @@ def reddit_scrap(word):
     target = reddit.subreddit("all")
 
     for i in target.search(word, limit=10000):
-        raw_list.append(i.title.split())
-        raw_list.append(i.selftext.split())
-    
+        if(len(i.title.split()) != 0):
+            raw_list.extend(i.title.split())
+        if(len(i.selftext.split()) != 0):
+            raw_list.extend(i.selftext.split())
         
+            
 
-#words_list = set(raw_list)
 reddit_scrap(criteria)
-#print(str(raw_list.__sizeof__()))
-#print(len(raw_list))
-#print(raw_list)
-
+frequency = collections.Counter(raw_list)
+'''''
 def freq(raw_list):
 
     for words in raw_list :
-        print('Frequency of ', words , 'is :', raw_list.count(words))
+        print("Frequency of"," '" ,  words , "'", "is :", raw_list.count(words))
  
 print(freq(raw_list))
+'''
+print(frequency.most_common(100))
